d87d7fa70e7e2c60d6bb959bbad1cbc4ab3417e7
pinki84 <oktawian1@yahoo.co.uk>
2020-07-07 19:41:36 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,29 @@
+from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_file_handler
-
+from bs4 import BeautifulSoup
+import requests
 
 class Lotterynumbers(MycroftSkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+        super(Lotterynumbers, self).__init__(name="Lotterynumbers")
 
-    @intent_file_handler('lotterynumbers.intent')
+    @intent_handler(IntentBuilder("Lotterynumbers").require("lottery"))
     def handle_lotterynumbers(self, message):
-        self.speak_dialog('lotterynumbers')
+        session = requests.get('https://www.national-lottery.co.uk/results/euromillions/draw-history/xml')
+        soup = BeautifulSoup(session.content, 'lxml-xml')
+
+        for ball in soup.find_all('ball'):
+            print(ball.text)
+            self.speak_dialog(ball.text)
+         
+        for url in soup.find_all('bonus-ball'):
+         
+            self.speak_dialog(url.text)
+       
+        for url in soup.find_all('raffle'):
+            self.speak_dialog(url.text)
+                  
+        #self.speak_dialog('lotterynumbers')
 
 
 def create_skill():
